2376a4438dfeace9446eb3e8785130355810bbbb
anuloma <genu.ged@gmail.com>
2016-09-06 14:33:38 +0300

    Files

--- ip_addr.c
+++ ip_addr.c
@@ -6,17 +6,20 @@
 struct ip_addr
 {
   unsigned char ip[4];
+  unsigned short port;
   struct ip_addr *next;
 };
 
 void 
-append_address(struct ip_addr **head, int ip[4])
+append_address(struct ip_addr **head, int ip[4], unsigned short port)
 {
   struct ip_addr *curr = (struct ip_addr *)malloc(sizeof(struct ip_addr));
 
   int i;
   for (i = 0; i < 4; i++)
     curr->ip[i] = ip[i];
+
+  curr->port = port;
   curr->next = *head;
   *head = curr;
 }
@@ -27,8 +30,10 @@ print_address(struct ip_addr *head)
   struct ip_addr *curr = head;
   while (curr != NULL)
   {
-    printf("IPv4 address: %d.%d.%d.%d\n", 
-	   curr->ip[0], curr->ip[1], curr->ip[2], curr->ip[3]);
+    printf("IPv4 address: %d.%d.%d.%d:%d\n", 
+	   curr->ip[0], curr->ip[1], curr->ip[2], curr->ip[3],  
+           curr->port);
+
     curr = curr->next;
   }
 }
@@ -41,44 +46,101 @@ is_valid_ip(int ip_address[4])
           (ip_address[2] >= 0 && ip_address[2] <= 255) &&
           (ip_address[3] >  0 && ip_address[3] <= 255)); 
 }
+
+int
+read_number(const char *number_string, int *number)
+{
+  int nchar = 0;
+
+  *number = 0;
+  while (*number_string >= '0' && *number_string <= '9')
+  {
+    *number = *number * 10 + (*number_string - '0');
+    number_string++;
+    nchar++;
+  }
+  return nchar;
+}
+
 void 
 convert_to_number(const char *address)
 {
   struct ip_addr *addr = NULL;
   int temp_addr[4] = {-1, -1, -1, -1};
   int counter = 0;
-  
+  int is_port = 0, port = -1;
+  char prev = *address;
+
   while (*address)
   {
-    if (*address == '.')
-      counter++;
-    else if (*address == ',')
+    switch (*address)
     {
-      if (is_valid_ip(temp_addr))
+      case '.': 
+        if (prev == ',' || prev == '.' || prev == ':')
+        {
+          printf("Invalid address (.)\n");
+          return;
+        }
+        counter++; 
+        break;
+      case ',': 
+        if (prev == ',' || prev == '.' || prev == ':')
+        {
+          printf("Invalid address (,)\n");
+          return;
+        }
+
+        if (is_valid_ip(temp_addr) && port > 0)
+        {
+	  int i;
+          
+          append_address(&addr, temp_addr, port);
+          for (i = 0; i < 4; i++)
+            temp_addr[i] = -1;
+          port = -1;
+          counter = 0;	  
+          is_port = 0;
+        }
+        break;
+     case '0':
+     case '1':
+     case '2':
+     case '3':
+     case '4':
+     case '5':
+     case '6':
+     case '7':
+     case '8':
+     case '9':
+     {
+       int nchar;
+       if (is_port)
+         nchar = read_number(address, &port);
+       else
+         nchar = read_number(address, &temp_addr[counter]);
+       address += nchar - 1;
+       printf("%d\n", temp_addr[counter]);
+       break;
+     }
+    case ':': 
+      if (prev == '.' || prev == ',' || prev == ':' || is_port == 1)
       {
-        int i;
-        append_address(&addr, temp_addr);
-        for (i = 0; i < 4; i++)
-          temp_addr[i] = -1;
-        counter = 0;
-      }
-      else 
-      {
-        printf("Invalid IP address\n");
+        printf("Invalid port number\n");
         return;
       }
-    }
-    else if (*address >= '0' && *address <= '9')
-    {
-      if (temp_addr[counter] == -1)
-        temp_addr[counter] = *address - '0';
-      else temp_addr[counter] = temp_addr[counter] * 10 + (*address - '0');
-    }
+      is_port = 1;
+      break;
+    default: 
+      printf("Invalid characters detected\n");
+      return;
+    }   
+    prev = *address;
     address++;
-  }
+  }  
 
-  if (is_valid_ip(temp_addr))
-    append_address(&addr, temp_addr);
+  if (is_valid_ip(temp_addr) && port > 0)
+    append_address(&addr, temp_addr, port);
+
   print_address(addr);
 }
 
